2ed85a9bdc13e8bce21ac0b12248bb0fb16efcdc
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-10-14 19:16:31 -0300

    feat: add manifest manager Added `new` method for instantiating a Manifest struct, parsing the manifest from a given path; Added `generate_html_tags` method to get the assets html tags from the given entries.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 mod error;
 mod asset;
 mod chunk;
+mod manifest;
---
+++ src/manifest.rs
@@ -0,0 +1,73 @@
+use std::fs::File;
+use std::collections::{HashMap, HashSet};
+use std::io::BufReader;
+use serde::Deserialize;
+
+use crate::asset::Asset;
+use crate::chunk::Chunk;
+use crate::error::{ViteError, ViteErrorKind};
+
+#[derive(Deserialize, Debug)]
+#[serde(transparent)]
+pub(crate) struct Manifest {
+    manifest: HashMap<String, Chunk>,
+}
+
+impl Manifest {
+    pub fn new(path: &str) -> Result<Self, ViteError> {
+        let file = match File::open(path) {
+            Err(err) => {
+                return Err(ViteError::new(
+                    format!("Failed to open manifest at {}: {}", path, err),
+                    ViteErrorKind::Manifest
+                ));
+            },
+            Ok(file) => file,
+        };
+
+        let reader = BufReader::new(file);
+        let manifest = serde_json::from_reader(reader);
+        
+        return match manifest {
+            Err(err) => Err(ViteError::new(
+                format!("Failed to parse manifest json: {}", err),
+                ViteErrorKind::Manifest
+            )),
+            Ok(manifest) => Ok(manifest)
+        };
+    }
+
+    pub fn generate_html_tags(&self, entrypoints: Vec<&str>) -> String {
+        if self.manifest.is_empty() {
+            log::warn!("Manifest is empty. Empty string being returned from `Manifest::generate_html_tags`.");
+            return "".into();
+        }
+
+        let mut discovered_assets = HashSet::<Asset>::new();
+
+        entrypoints
+            .into_iter()
+            .for_each(|entry| {
+                let entry_as_asset = Asset::EntryPoint(entry.to_string());
+                if discovered_assets.contains(&entry_as_asset) { return; }
+                discovered_assets.insert(entry_as_asset);            
+                
+                let chunk: &Chunk = &self.manifest[entry];
+                for asset in chunk.assets_iter() {
+                    if !discovered_assets.contains(&asset) {
+                        discovered_assets.insert(asset);
+                    }
+                }
+            });
+
+        let mut assets = discovered_assets.into_iter().collect::<Vec<Asset>>();
+        // Puts the assets in the following order: stylesheets > entries > preloads
+        assets.sort();
+
+        return assets
+            .into_iter()
+            .map(|asset| asset.to_html())
+            .collect::<Vec<String>>()
+            .join("\n");
+    }
+}
