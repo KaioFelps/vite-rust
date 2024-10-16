83191ea9786de13117ae5021c894db278ad8e22e
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-10-16 00:51:46 -0300

    feat: expose a function for resolving manifest at lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
-mod error;
+pub mod error;
 mod asset;
 mod chunk;
 mod manifest;
+
+use error::ViteError;
+use manifest::Manifest;
+
+pub fn resolve_manifest(manifest_path: &str, entries: Vec<&str>) -> Result<String, ViteError> {
+    let manifest = Manifest::new(manifest_path)?;
+    let html = manifest.generate_html_tags(entries);
+    return Ok(html);
+}
