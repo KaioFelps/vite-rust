cf786192e4431c740112c872398a10fc76b3cf3b
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-10-19 22:40:03 -0300

    chore: experimenting Vite struct at example application

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,10 +1,23 @@
-fn main() {
+use vite_rust::ViteConfig;
+
+#[tokio::main]
+async fn main() {
     env_logger::init();
 
-    let vite_resolved = vite_rust::resolve_manifest(
-        "example/dist/.vite/manifest.json",
-        vec!["src/www/main.tsx", "src/www/index.css"]
-    );
+    let mut vite_config = ViteConfig::new_with_defaults("example/dist/.vite/manifest.json");
+    vite_config.entrypoints = Some(vec!["src/www/main.tsx", "src/www/index.css"]);
+    vite_config.force_mode = Some(vite_rust::ViteMode::Manifest);
 
-    println!("{}", vite_resolved.unwrap());
+    let vite = vite_rust::Vite::new(vite_config).await;
+
+    if vite.is_err() {
+        panic!("{:#?}", vite.unwrap_err());
+    }
+    
+    let vite = vite.unwrap();
+
+    let vite_resolved = vite.get_tags();
+
+    println!("{}", vite_resolved);
+    println!("{}", vite.get_hash());
 }
