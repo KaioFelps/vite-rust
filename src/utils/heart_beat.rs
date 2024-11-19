6c64b39e8d75450244784af44c1542ebe972b254
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-11-19 20:10:24 -0300

    feat: heart beat checker retries Now, heartbeat checker will retry five times before fallbacking to `Manifest` mode.

--- src/utils/heart_beat.rs
+++ src/utils/heart_beat.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{future::Future, pin::Pin, time::Duration};
 
 use crate::CLIENT_SCRIPT_PATH;
 
@@ -11,30 +11,79 @@ pub(crate) async fn check_heart_beat(host: &str, timeout: Option<Duration>) -> b
     let ping_endpoint = match host.ends_with("/") {
         true => {
             let mut host = host.to_string();
-            host.push_str(&CLIENT_SCRIPT_PATH);
-            host
-        },
+            host.push_str(CLIENT_SCRIPT_PATH);
+            host.into_boxed_str()
+        }
         false => {
             let mut host = host.to_string();
             host.push('/');
-            host.push_str(&CLIENT_SCRIPT_PATH);
-            host
-        },
+            host.push_str(CLIENT_SCRIPT_PATH);
+            host.into_boxed_str()
+        }
     };
 
-    let response = reqwest::Client::new()
-        .get(ping_endpoint)
-        .timeout(timeout)
-        .send()
-        .await;
+    let response = retry_cb(5, move || {
+        let ping_endpoint = ping_endpoint.clone().to_string();
+        Box::pin(async move {
+            reqwest::Client::new()
+                .get(ping_endpoint.as_str())
+                .timeout(timeout)
+                .send()
+                .await
+        })
+    })
+    .await;
 
     match response {
         Err(err) => {
-            log::error!("Failed to make HTTP request to heartbeat-check endpoint: {}", err);
-            return false;
-        },
-        Ok(response) => {
-            return response.status() == 200;
+            log::error!(
+                "Failed to make HTTP request to heartbeat-check endpoint: {}",
+                err
+            );
+            false
         }
-    };
+        Ok(response) => response.status() == 200,
+    }
+}
+
+async fn retry_cb<T, E>(
+    mut retries_count: i32,
+    cb: impl Send + Sync + 'static + Fn() -> Pin<Box<dyn Future<Output = Result<T, E>>>> + 'static,
+) -> Result<T, E> {
+    let mut response = cb().await;
+
+    while response.is_err() && retries_count > 0 {
+        response = cb().await;
+        retries_count -= 1;
+    }
+
+    response
+}
+
+#[cfg(test)]
+mod test {
+    use super::retry_cb;
+    use std::sync::{Arc, Mutex};
+
+    #[tokio::test]
+    async fn test_retry_cb() {
+        let tries_count = Arc::new(Mutex::new(0));
+        let tries_clone = Arc::clone(&tries_count);
+
+        let result = retry_cb(5, move || {
+            let tries_count = tries_clone.clone();
+            Box::pin(async move {
+                let count = *tries_count.lock().unwrap();
+                if count > 0 {
+                    *tries_count.lock().unwrap() = count + 1;
+                    return Err(());
+                }
+
+                Ok(())
+            })
+        })
+        .await;
+
+        assert!(result.is_ok());
+    }
 }
